ef8f9f5c86df801a1fc8b715640497644dc9dcfe
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-07 11:21:20 +0000

    3.1+ compatibility update
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/footer-content@2044 f53fe46e-8c02-459b-9fdd-93e75771b278

--- footer-content.php
+++ footer-content.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Footer Content
-Plugin URI: 
-Description:
+Plugin URI: http://premium.wpmudev.org/project/footer-content
+Description: This plugin allows blog administrators to add their own content to the footer of every page on their blog
 Author: Andrew Billits (Incsub)
 Version: 1.0.1
-Author URI:
+Author URI: http://premium.wpmudev.org/
 WDP ID: 76
 */
 
@@ -29,11 +29,19 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //------------------------------------------------------------------------//
 //---Hook-----------------------------------------------------------------//
 //------------------------------------------------------------------------//
+add_action('init', 'footer_content_init');
 add_action('admin_menu', 'footer_content_plug_pages');
 add_action('wp_footer', 'footer_content_output');
 //------------------------------------------------------------------------//
 //---Functions------------------------------------------------------------//
 //------------------------------------------------------------------------//
+
+function footer_content_init() {
+	if ( !is_multisite() )
+		exit( 'The Footer Content plugin is only compatible with WordPress Multisite.' );
+		
+	load_plugin_textdomain('footer_content', false, dirname(plugin_basename(__FILE__)).'/languages');
+}
 
 function footer_content_output() {
 	$footer_content = get_option('footer_content');
@@ -45,7 +53,7 @@ function footer_content_output() {
 function footer_content_plug_pages() {
 	global $wpdb, $wp_roles, $current_user;
 	if ( is_site_admin() ) {
-		add_submenu_page('themes.php', 'Footer Content', 'Footer Content', 10, 'footer-content', 'footer_content_page_output');
+		add_submenu_page('themes.php', __('Footer Content', 'footer_content'), __('Footer Content', 'footer_content'), 10, 'footer-content', 'footer_content_page_output');
 	}
 }
 
@@ -57,32 +65,32 @@ function footer_content_page_output() {
 	global $wpdb, $wp_roles, $current_user;
 	
 	if(!current_user_can('manage_options')) {
-		echo "<p>" . __('Nice Try...') . "</p>";  //If accessed properly, this message doesn't appear.
+		echo "<p>" . __('Nice Try...', 'footer_content') . "</p>";  //If accessed properly, this message doesn't appear.
 		return;
 	}
 	if (isset($_GET['updated'])) {
-		?><div id="message" class="updated fade"><p><?php _e('' . urldecode($_GET['updatedmsg']) . '') ?></p></div><?php
+		?><div id="message" class="updated fade"><p><?php _e(urldecode($_GET['updatedmsg']), 'footer_content') ?></p></div><?php
 	}
 	echo '<div class="wrap">';
 	switch( $_GET[ 'action' ] ) {
 		//---------------------------------------------------//
 		default:
 			?>
-			<h2><?php _e('Footer Content') ?></h2>
-            <p><?php _e('The footer content is displayed at the bottom of every page on your blog'); ?></p>
+			<h2><?php _e('Footer Content', 'footer_content') ?></h2>
+            <p><?php _e('The footer content is displayed at the bottom of every page on your blog', 'footer_content'); ?></p>
             <form method="post" action="themes.php?page=footer-content&action=process">
             <table class="form-table">
             <tr valign="top">
-            <th scope="row"><?php _e('Footer Content') ?></th>
+            <th scope="row"><?php _e('Footer Content', 'footer_content') ?></th>
             <td>
             <textarea name="footer_content" type="text" rows="5" wrap="soft" id="footer_content" style="width: 95%"/><?php echo get_option('footer_content') ?></textarea>
-            <br /><?php _e('HTML allowed') ?></td>
+            <br /><?php _e('HTML allowed', 'footer_content') ?></td>
             </tr>
             </table>
             
             <p class="submit">
-            <input type="submit" name="Submit" value="<?php _e('Save Changes') ?>" />
-			<input type="submit" name="Reset" value="<?php _e('Reset') ?>" />
+            <input type="submit" name="Submit" value="<?php _e('Save Changes', 'footer_content') ?>" />
+			<input type="submit" name="Reset" value="<?php _e('Reset', 'footer_content') ?>" />
             </p>
             </form>
 			<?php
@@ -93,14 +101,14 @@ function footer_content_page_output() {
 				update_option( "footer_content", "" );
 				echo "
 				<SCRIPT LANGUAGE='JavaScript'>
-				window.location='themes.php?page=footer-content&updated=true&updatedmsg=" . urlencode(__('Settings cleared.')) . "';
+				window.location='themes.php?page=footer-content&updated=true&updatedmsg=" . urlencode(__('Settings cleared.', 'footer_content')) . "';
 				</script>
 				";			
 			} else {
 				update_option( "footer_content", stripslashes($_POST[ 'footer_content' ]) );
 				echo "
 				<SCRIPT LANGUAGE='JavaScript'>
-				window.location='themes.php?page=footer-content&updated=true&updatedmsg=" . urlencode(__('Settings saved.')) . "';
+				window.location='themes.php?page=footer-content&updated=true&updatedmsg=" . urlencode(__('Settings saved.', 'footer_content')) . "';
 				</script>
 				";
 			}
@@ -113,4 +121,12 @@ function footer_content_page_output() {
 	echo '</div>';
 }
 
-?>
+if ( !function_exists( 'wdp_un_check' ) ) {
+	add_action( 'admin_notices', 'wdp_un_check', 5 );
+	add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+
+	function wdp_un_check() {
+		if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+			echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+	}
+}
